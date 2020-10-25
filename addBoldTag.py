75c76715f2240127a4a4b669372a0def07974b97
HaoWeiHe <altar396@gmail.com>
2020-10-25 17:43:37 +0800

    616. Add Bold Tag in String

---
+++ addBoldTag.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def addBoldTag(self, s, dict):
+        """
+        :type s: str
+        :type dict: List[str]
+        :rtype: str
+        """
+        bold = [0 for _ in range(len(s))]
+        for i in range(len(s)):
+            for ele in dict:
+                lenght = len(ele)
+                if "".join(s[i : i+lenght]) == ele:
+                    bold[i:i+lenght] = [1]* lenght
+        res = ""
+        #Begin:[,..0,1,..], [1,..]
+        #end:[..,1,0] , [1,1,1]
+        flag_0, flag_1 = False, False
+        for idx, val in enumerate(bold):
+            if idx ==0 and val ==1: 
+                res += "<b>"
+            if flag_0 and val ==1: 
+                res += "<b>"
+            if flag_1 and val ==0:
+                res += "</b>"
+            res += s[idx]
+            if idx == len(s)-1 and val == 1:
+                res += "</b>"
+
+            flag_0 = val == 0
+            flag_1 = val ==1
+        return res
