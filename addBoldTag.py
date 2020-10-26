4ca68a67759b09bd66f8871387004ed612729054
HaoWeiHe <altar396@gmail.com>
2020-10-26 10:27:51 +0800

    sort up files into alg and lc

--- addBoldTag.py
+++ addBoldTag.py
@@ -6,11 +6,18 @@ class Solution(object):
         :rtype: str
         """
         bold = [0 for _ in range(len(s))]
+        dic = collections.defaultdict(set)
+        for ele in dict:
+            dic[len(ele)].add(ele)
+        
+
         for i in range(len(s)):
-            for ele in dict:
-                lenght = len(ele)
-                if "".join(s[i : i+lenght]) == ele:
-                    bold[i:i+lenght] = [1]* lenght
+            for length, sets in dic.items():
+                if "".join(s[i:i+length]) in sets:
+                    bold[i:i+length]  = [1]*length
+                # lenght = len(ele)
+                # if "".join(s[i : i+lenght]) == ele:
+                #     bold[i:i+lenght] = [1]* lenght
         res = ""
         #Begin:[,..0,1,..], [1,..]
         #end:[..,1,0] , [1,1,1]
