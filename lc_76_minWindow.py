1514e2f0170e83d83871a0224d94b6ca1f854b21
HaoWeiHe <altar396@gmail.com>
2020-10-28 16:53:20 +0800

    opt check function with extra memo, time complex reduce from O(n) -> O(1) for every iterat

--- lc_76_minWindow.py
+++ lc_76_minWindow.py
@@ -3,41 +3,53 @@ import collections
 class Solution(object):
     def minWindow(self, s, t):
         """
-        consider this quesiotn as LRU, but only put
-        insert: when ele in T
-        capacity <-> check_if_full
         
-        S = "ADOBECODEBANC", 
-        T = "ABC"
+        #    S = "ADOBECODEBANC", 
+        #    T = "ABC"
 
-        ADOBECODEB  A  N  C
-        0123456789  10 11 12
-        A  B C   B   
-
-lst   [ABC]   [C B] [CBA]    [BA] [BAC] 
-      [0,3,5] [5,9] [5,9,10] [9,10] [9,10,12]
-len   6        5       6             4
-        
-        if hit:
-            orderest: remove to end 
-            
-        if full
-            count firstele and lastele in ordered
-            remove first 
-            update mappingLst: remove first
-        
+        if still find keep looking
 
         """
+
+#    def minWindow2(self, s, t):
+
+#         """
+#         consider this quesiotn as LRU, but only put
+#         insert: when ele in T
+#         capacity <-> check_if_full
+        
+#         S = "ADOBECODEBANC", 
+#         T = "ABC"
+
+#         ADOBECODEB  A  N  C
+#         0123456789  10 11 12
+#         A  B C   B   
+
+# lst   [ABC]   [C B] [CBA]    [BA] [BAC] 
+#       [0,3,5] [5,9] [5,9,10] [9,10] [9,10,12]
+# len   6        5       6             4
+        
+#         if hit:
+#             orderest: remove to end 
+            
+#         if full
+#             count firstele and lastele in ordered
+#             remove first 
+#             update mappingLst: remove first
+        
+#         """
         C = collections.Counter(t)
-        def is_full(d):
-            lst = [s[e] for e in d]
-            d_to_freq = collections.Counter(lst)
+     
+        def is_full(window_counts):
+            # lst = [s[e] for e in d ]
+            # window_counts = collections.Counter(lst)
             
             for key in C:    
-                if key not in d_to_freq or d_to_freq[key] < C[key]: return False
+                if key not in window_counts or window_counts[key] < C[key]: return False
             return True
         
         d = collections.OrderedDict()
+        window_counts = collections.defaultdict(int)
         s = s
         t = set(t)
         lst = []
@@ -46,11 +58,13 @@ len   6        5       6             4
             if c in t:
                 if c not in d:
                     d[idx] = 1
+
                 else:
                     d.move_to_end(idx)
+                window_counts[c]  += 1
             
-            while is_full(d):
-                # orderedlst = list(d)
+            while is_full(window_counts):
+                
                 end_idx = d.popitem(last = True)[0]
                 d[end_idx] = 1
                 first_idx = next(iter(d))
@@ -58,6 +72,11 @@ len   6        5       6             4
                 if minLength > end_idx -first_idx + 1:
                     minLength = end_idx -first_idx + 1
                     res = s[first_idx: end_idx + 1]
-                d.popitem(last = False)
+                frist_item = d.popitem(last = False)
+                window_counts[s[frist_item[0]]] -= 1
                 
         return res
+
+s,t = "ADOBECODEBANC", "ABC"
+a = Solution().minWindow(s,t)
+print(a)
