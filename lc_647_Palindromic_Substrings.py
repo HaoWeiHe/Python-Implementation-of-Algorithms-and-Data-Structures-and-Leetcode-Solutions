ef28f46a64a9ef90f8cc3d9cfbf568f2b8390d34
HaoWeiHe <altar396@gmail.com>
2020-11-08 08:42:55 +0800

    central extension

--- lc_647_Palindromic_Substrings.py
+++ lc_647_Palindromic_Substrings.py
@@ -1,5 +1,21 @@
+
 class Solution(object):
     def countSubstrings(self, s):
+        N = len(s)
+        res = 0
+        for center in xrange(2*N - 1):
+            # aba 3/2 1, 2
+            # abba 4/2, 2, 2
+            lf = center /2
+            rt = lf + center%2
+            while lf >=0 and rt < N and s[lf] == s[rt]:
+                res +=1 
+                lf -=1
+                rt +=1
+        return res
+
+
+    def countSubstrings_sol1(self, s):
         """
         :type s: str
         :rtype: int
@@ -18,9 +34,12 @@ class Solution(object):
         for i in range(n):
             for j in range(i+1,n+1):
                 if s[i:j] in record:
-                    res +=1# record[s[i:j]]
+                    res +=1
                 else:
                     if isPalindromic(s[i:j]):
                         res += 1
                         record.add(s[i:j])        
         return res
+
+s = "aba"
+Solution().countSubstrings(s)
