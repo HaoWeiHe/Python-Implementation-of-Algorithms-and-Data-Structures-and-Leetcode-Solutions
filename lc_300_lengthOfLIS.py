6f39e4acd722f9814f4e5811ed06aab43736ec49
HaoWeiHe <altar396@gmail.com>
2020-11-15 14:06:21 +0800

    update

--- lc_300_lengthOfLIS.py
+++ lc_300_lengthOfLIS.py
@@ -1,9 +1,30 @@
+from  bisect import bisect_left
+
 class Solution(object):
     def lengthOfLIS(self, nums):
         """
         dp[i] = the longest increasing subsequ from 0 -> i 
         [10,9,2,5,3,7,101,18]
                     i
+
+        """
+       
+        dp = [0] * len(nums)
+        res = 0
+        for i in range(len(nums)):
+            insert_idx = bisect_left(dp, nums[i], hi= res)
+            print(dp,insert_idx, nums[i])
+            dp[insert_idx] = nums[i]
+            if insert_idx == res:
+                res += 1
+        return res
+        
+    def lengthOfLIS2(self, nums):
+        """
+        dp[i] = the longest increasing subsequ from 0 -> i 
+        [10,9,2,5,3,7,101,18]
+                    i
+        
          j j j j jj           
         [1 ,1,1,2,2,3, 4,4 ]
         """
@@ -17,3 +38,5 @@ class Solution(object):
                     dp[i] = max(dp[i], dp[j] + 1)
         
         return max(dp)
+ns =  [10,9,2,5,3,7,101,18]
+Solution().lengthOfLIS(ns)
