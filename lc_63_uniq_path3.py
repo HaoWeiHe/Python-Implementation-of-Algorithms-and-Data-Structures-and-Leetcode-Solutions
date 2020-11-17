aa8cabeb530f88f95effb036f1313d0bd60e8882
HaoWeiHe <altar396@gmail.com>
2020-11-17 16:09:47 +0800

    dp /w O(1) space

--- lc_63_uniq_path3.py
+++ lc_63_uniq_path3.py
@@ -1,5 +1,77 @@
 class Solution(object):
     def uniquePathsWithObstacles(self, g):
+        if not g: 
+            return 0
+        if g[0][0] == 1:
+            return 0
+        m, n = len(g), len(g[0])
+
+        flag = True
+        for i in range(m):
+            if g[i][0] == 0 and flag:
+                g[i][0] = 1   
+            else:
+                flag = False
+                g[i][0] = 0
+
+        flag = True
+        for j in range(1,n):
+            if g[0][j] == 0 and flag:
+                g[0][j] = 1 
+                
+            else:
+                flag = False
+                g[0][j] = 0
+
+        for i in range(1,m):
+            for j in range(1,n):
+                if g[i][j] ==0:
+                    g[i][j] = g[i-1][j] + g[i][j-1]
+                else:
+                    g[i][j] = 0
+      
+        return g[-1][-1]
+
+    def uniquePathsWithObstacles2(self, g):
+        """
+        
+        f(x,y) = f(x-1,y) + f(x, y-1)
+       
+        """
+        if not g: 
+            return 0
+        if g[0][0] == 1:
+            return 0
+
+        m, n = len(g), len(g[0])
+        dp = [[0] * n for _ in range(m)]
+        
+        flag = True
+        for i in range(m):
+            if g[i][0] == 0 and flag:
+                dp[i][0] = 1   
+            else:
+                flag = False
+                dp[i][0] = 0
+
+        flag = True
+        for j in range(n):
+            if g[0][j] == 0 and flag:
+                dp[0][j] = 1 
+                
+            else:
+                flag = False
+                dp[0][j] = 0
+
+        for i in range(1,m):
+            for j in range(1,n):
+                if g[i][j] ==0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+ 
+        return dp[-1][-1]
+
+        
+    def uniquePathsWithObstacles3(self, g):
         """
         
         f(x,y) = f(x-1,y) + f(x, y-1)
@@ -22,5 +94,9 @@ class Solution(object):
             return self.mem[(x,y)]
 
         return f(m-1,n-1)
+
+
+g  =[[0,0,0],[0,1,0],[0,0,0]]
+Solution().uniquePathsWithObstacles(g)
 
         
