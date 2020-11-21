95639f19176eaa24c8697716d999f93490181ded
Wei <13915371+HaoWeiHe@users.noreply.github.com>
2020-11-21 08:45:17 +0800

    Update lc_413ArithmeticSlices.py
    add dp solution

--- lc_413ArithmeticSlices.py
+++ lc_413ArithmeticSlices.py
@@ -1,6 +1,21 @@
 class Solution(object):
     def numberOfArithmeticSlices(self, A):
         """
+        dp[i] = dp[i-1] + 1 if follow the rule
+        
+        """
+        
+        dp = [0]*len(A) 
+        res = 0
+        for i in range(2,len(A)):
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]):
+                dp[i] = dp[i - 1] + 1;
+            res += dp[i]
+      
+        return res
+    
+    def numberOfArithmeticSlices2(self, A):
+        """
         
         [7, 7, 7, 1, 3, 5, 7, 9]
     gap [_, 0, 0,-6 2  2  2. 2 ]
