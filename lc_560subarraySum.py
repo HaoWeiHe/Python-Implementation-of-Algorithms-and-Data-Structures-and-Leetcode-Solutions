66d6e6416eaa78b1ab7d1ab207954cb868dfe5a3
HaoWeiHe <altar396@gmail.com>
2020-12-14 16:34:52 +0800

    another approach, use more time complex but save space complex(from O(n) to O(1))

--- lc_560subarraySum.py
+++ lc_560subarraySum.py
@@ -1,5 +1,31 @@
+
+import collections
 class Solution(object):
     def subarraySum(self, nums, k):
+        """
+        [3,4,7,2,-3,1,4,2] , k = 7
+    acc  3 7 14
+        [1,2,3]
+         1 3 6
+    ans  0 1 3
+        """
+        acc,ans = 0,0
+        for idx, e in enumerate(nums):
+            acc += e
+            if acc == k: 
+                ans +=1 
+
+            tmp = acc
+            for j in xrange(idx):
+                tmp -= nums[j]
+                if tmp ==k:
+                    ans +=1
+        
+        return ans
+
+
+
+    def subarraySum2(self, nums, k):
         """
          [3,4,7,2,-3,1,4,2] , k = 7
     h =  {0:1, 3:1, 7:1, 14:2, 16:1, 13:1}
@@ -17,4 +43,6 @@ acc-k=   -4,0 7   9  6 7
             ans += h[acc -k ]
             h[acc] +=1
         return ans
-        
+nums  = [1,2,3]
+k = 3
+Solution().subarraySum(nums,k)
