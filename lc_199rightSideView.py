3bbeabb8f4bfba1d40995e78701b864a2ba11370
HaoWeiHe <altar396@gmail.com>
2020-12-15 20:24:22 +0800

    use dfs method /w same logic

--- lc_199rightSideView.py
+++ lc_199rightSideView.py
@@ -1,11 +1,29 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
 class Solution(object):
     def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        res =[]
+        
+        def dfs(root,lvl):
+            if len(res) == lvl:
+                res.append(root.val)
+            else:
+                res[lvl] = root.val
+            if root.left:
+                dfs(root.left,  lvl+1)
+            if root.right:
+                dfs(root.right, lvl+1)
+                
+        dfs(root,0)
+        return res
+
+    def rightSideView2(self, root):
         """
         
         """
