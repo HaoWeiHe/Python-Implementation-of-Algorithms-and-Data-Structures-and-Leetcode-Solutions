37299cd2ca133dbb8c414370fad0d084d4548ac5
HaoWeiHe <altar396@gmail.com>
2021-01-10 16:42:44 +0800

    use idx to compute the result

---
+++ lc_2570SparseVector.py
@@ -0,0 +1,23 @@
+class SparseVector:
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.v = nums
+        
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec):
+        """
+        :type vec: 'SparseVector'
+        :rtype: int
+        """
+        res = 0 
+        for i in range(len(self.v)):
+            res += self.v[i]*vec.v[i]
+        
+        return res
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
