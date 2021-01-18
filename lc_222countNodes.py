501b6acdeff006a3e6fff335ea2a52accf3fce90
HaoWeiHe <altar396@gmail.com>
2021-01-18 17:07:39 +0800

    helperfuntion:check the depth of right most and the depth left most. If depth(right_most) == depth(left_most) return its counts, else return 1+dfs(root.right)+dfs(root.left

--- lc_222countNodes.py
+++ lc_222countNodes.py
@@ -1,11 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
 class Solution(object):
     def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def countDepth(root,right):
+            if not root:return 0
+            if right:
+                return 1+ countDepth(root.right, right)
+            else:
+                return 1+ countDepth(root.left, right)
+        def dfs(root):
+            if not root:return 0
+            r_dth = countDepth(root.right,1) 
+            l_dth = countDepth(root.left,0)
+            if r_dth == l_dth :
+                return pow(2, r_dth+1)-1
+            else:
+                return 1 + dfs(root.right) + dfs(root.left)
+        return dfs(root)
+    def countNodes2(self, root):
         """
         :type root: TreeNode
         :rtype: int
