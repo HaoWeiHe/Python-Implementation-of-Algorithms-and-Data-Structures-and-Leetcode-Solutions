430b77e3f314b3ee83bf547fbc186f81b14883d2
HaoWeiHe <altar396@gmail.com>
2021-01-18 16:02:08 +0800

    take this quesiton as findding a number in bst, but always compute the dis

--- lc_270closestValue.py
+++ lc_270closestValue.py
@@ -7,6 +7,25 @@
 class Solution(object):
     def closestValue(self, root, target):
         """
+        go right if cur is larger than target
+        go left if cur is less than target
+        always cmp 
+        """
+        self.res, self.dis = None, float('inf')
+        
+        def dfs(root):
+            if not root:return 
+            if self.dis > abs(root.val - target):
+                self.dis = abs(root.val - target)
+                self.res = root.val
+            if root.val > target:
+                dfs(root.left)
+            else:
+                dfs(root.right)
+        dfs(root)
+        return self.res
+    def closestValue2(self, root, target):
+        """
         :type root: TreeNode
         :type target: float
         :rtype: int
