bac42d0c4040339480b184dd85985d3839160f64
HaoWeiHe <altar396@gmail.com>
2021-01-22 17:12:28 +0800

    use nxt pre cur pointer to implement

--- lc_206reverseList.py
+++ lc_206reverseList.py
@@ -3,23 +3,18 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution(object):
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
-        1->2->3->4->5->NULL
-                         ^
-        d ->5->4->3-> 2->1
-        return d.next
         """
-        d = ListNode()
-        d.next = None
-        while head:
-            nxt = head.next
-            tmp = d.next
-            d.next = head
-            head.next = tmp
-            head = nxt
-        return d.next
+        pre, cur= None, head
+        
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+        return pre
+            
