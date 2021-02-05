7e42cb1e51c0290084528e093bb403779d630037
HaoWeiHe <altar396@gmail.com>
2021-02-05 17:05:55 +0800

    /w orderdict

--- lc_340lengthOfLongestSubstringKDistinct.py
+++ lc_340lengthOfLongestSubstringKDistinct.py
@@ -1,6 +1,30 @@
 class Solution(object):
     def lengthOfLongestSubstringKDistinct(self, s, k):
         """
+        eceba
+        01234
+            i
+        {e:2,b = 3}
+        lf = first_item + 1 = 2
+        max = max(3,3-2+1 = 2)
+        {e:2, b=3, a = 4}
+max =   123
+        """
+        j = 0 
+        res = 0 #min()
+        counter  = collections.OrderedDict()
+        for i,ele in enumerate(s):
+            if ele in counter:
+                del counter[ele]
+            counter[ele] = i
+            if len(counter) > k:
+                _, del_idx = counter.popitem(last = False)
+                j = del_idx+1
+            if len(counter) <= k:
+                res= max(res, i-j+1)
+        return res
+    def lengthOfLongestSubstringKDistinct2(self, s, k):
+        """
         ["eceba"]
               i
             j
