8435b952199ebf7aa383b9b5cbd83f9ed08f2cba
HaoWeiHe <altar396@gmail.com>
2021-02-06 21:45:29 +0800

    use bisect_left method

--- lc_300lengthOfLIS1.py
+++ lc_300lengthOfLIS1.py
@@ -1,5 +1,52 @@
-
+from  bisect import bisect_left
 class Solution(object):
+    def insert(self, lst, n):
+        if not lst:
+            return 0
+
+        l, r = 0, len(lst) #(l,r]
+        while l < r:
+            m = (l+r)/2
+            if lst[m] > n: 
+                r = m
+            else:
+                l = m +1
+        return l - 1
+    def lengthOfLIS(self, nums):
+        """
+        [10,9,2,5,3,7,101,18]
+    lst=[10] 
+    lst=[9]
+    lst=[2]
+    lst=[2,5]
+        """
+        lst = []
+        res = 0
+        for e in nums:
+            # idx = self.insert(lst,e)
+            idx = bisect_left(lst, e)
+            if idx >= len(lst):
+                lst.append(e)
+            else:
+                lst[idx] = e
+        return len(lst)
+
+
+    def lengthOfLIS2(self, nums):
+        """
+        [10,9,2,5,3,7,101,18]
+          1 1 1 2 2 3  4   
+        """
+        h = {}
+        for i,n in enumerate(nums):
+            tmp = 0
+            for ele in h:
+                if ele < n:
+                    tmp = max(tmp, h[ele])
+            h[n] = tmp + 1
+        
+        return max(h.values())
+
     def insert(self, lst, target):
 
         l, r = 0, len(lst) #(l,r]
@@ -10,7 +57,7 @@ class Solution(object):
             else:
                 l = m +1
         return l 
-    def lengthOfLIS(self, nums):
+    def lengthOfLIS3(self, nums):
         """
         [10,9,2,5,3,7,101,18]
     lst=[10] 
@@ -32,20 +79,4 @@ class Solution(object):
         return len(lst)
 
 
-    def lengthOfLIS2(self, nums):
-        """
-        [10,9,2,5,3,7,101,18]
-          1 1 1 2 2 3  4   
-        """
-        h = {}
-        for i,n in enumerate(nums):
-            tmp = 0
-            for ele in h:
-                if ele < n:
-                    tmp = max(tmp, h[ele])
-            h[n] = tmp + 1
-        
-        return max(h.values())
-
-lst = [10,9,2,5,3,7,101,18]
-print(Solution().lengthOfLIS(lst))
+   
