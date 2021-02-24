27b6422d086659cb090558bf8015bc6d7852f3b9
HaoWeiHe <altar396@gmail.com>
2021-02-24 16:06:03 +0800

    use bst

--- lc_295MedianFinder.py
+++ lc_295MedianFinder.py
@@ -1,38 +1,48 @@
-
-
 class MedianFinder(object):
-
+    """
+    [1,2,3,4]
+     4/2 = 2 (cur, cur - 1)
+    [1,2,3]
+    3/2 = 1 (cur)
+    BST, to lst
+    if len(lst) is even, return avg(mid, mid-1)
+    else: return mid
+    
+    """
     def __init__(self):
         """
-        small half = maxheap
-        large half = minheap
-        select : to point which bucket shoule be increase new ele
-        
+        initialize your data structure here.
         """
-        self.small = []
-        self.large = []
-        
+        self.lst = []
+
     def addNum(self, num):
         """
         :type num: int
         :rtype: None
         """
-
-    def addNum(self, num):
-
-        if len(self.small) == len(self.large):
-            heappush(self.small, -heappushpop(self.large, num))
-        else:
-            heappush(self.large, -heappushpop(self.small, -num))
+        idx = bisect.bisect_left(self.lst, num)
+        # l, r = 0, len(self.lst) #[1:r)
+        # while l < r:
+        #     mid = (l+r) / 2
+        #     if num >= self.lst[mid]:
+        #         l = mid +1
+        #     else:
+        #         r = mid - 1
+        self.lst =self.lst[:idx] +   [num] + self.lst[idx:]
         
+
     def findMedian(self):
         """
         :rtype: float
         """
         
-        if len(self.small) == len(self.large):
-            return float(-self.small[0] + self.large[0])/2.0
-        return -self.small[0]
+        mid = len(self.lst)/2
+        if len(self.lst)%2 == 0 :
+            
+            return (self.lst[mid] + self.lst[mid-1])/2.0
+        else:
+            
+            return self.lst[mid]
         
 
 
