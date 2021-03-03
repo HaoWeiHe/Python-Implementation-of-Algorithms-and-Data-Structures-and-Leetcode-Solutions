7fbf273bd55017c1373ecb966a7427779f8c2afa
HaoWeiHe <altar396@gmail.com>
2021-03-03 17:21:08 +0800

    inverse int

--- lc_9isPalindrome.py
+++ lc_9isPalindrome.py
@@ -4,6 +4,24 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        if x < 0 or (x > 10 and x%10 == 0): return False
+        return x == self.reverse(x)
+    
+    def reverse(self, x):
+        res = 0 
+        
+        while x !=0:
+            tail = x %10
+            res = res* 10 + tail
+            x = x / 10 
+        
+        return res
+        
+    def isPalindrome2(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
         if x < 0:
             return False
         x = str(x)
@@ -14,3 +32,4 @@ class Solution(object):
             l +=1 
             r -=1
         return 1
+
