9ad46ede75600b9b070d3c8d10968d79f827eec9
HaoWeiHe <altar396@gmail.com>
2021-03-10 16:58:33 +0800

    use Floyd's

--- lc_287findDuplicate.py
+++ lc_287findDuplicate.py
@@ -1,11 +1,46 @@
 class Solution(object):
     def findDuplicate(self, nums):
         """
-        :type nums: List[int]
-        :rtype: int
-        """
-        h = set()
-        for e in nums:
-            if e in h: return e
-            h.add(e)
         
+        idxs  0,1,2,3,4
+        nums  1,3,4,2,2
+        
+        1 3 2 4 2 4 2 4 2
+        """
+        slow = fast = nums[0]
+      
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = nums[0]
+        
+        while slow != fast:
+            slow = nums[slow]
+            fast =nums[fast]
+        return fast
+        
+    def findDuplicate2(self, nums):
+        """
+        
+        idxs  0,1,2,3,4
+        nums  1,3,4,2,2
+        
+        1 3 2 4 2 4 2 4 2
+        """
+        slow = fast = nums[0]
+      
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = nums[0]
+        
+        while slow != fast:
+            slow = nums[slow]
+            fast =nums[fast]
+        return fast
+        
+
