3c578a7899a788fa461e436824c62beb339e358c
HaoWeiHe <altar396@gmail.com>
2021-03-16 16:50:36 +0800

    use bi-direction bfs

--- lc_127ladderLength.py
+++ lc_127ladderLength.py
@@ -1,3 +1,5 @@
+import collections
+
 class Solution(object):
     def ladderLength(self, beginWord, endWord, ws):
         """
@@ -18,19 +20,34 @@ class Solution(object):
             for idx in range(len(w)):
                 c = w[:idx] + "x" + w[idx+1:]
                 g[c].append(w)
-        q = deque([(beginWord,1)])
-        v = set()
-        while q:
+
+
+        def helper(q, visted, other_visted):
             top, lvl = q.popleft()
-           
             for idx in range(len(top)):
-                
                 c = top[:idx] + "x" + top[idx+1:]
-                if c in v:continue
-                v.add(c)
-                if c in g:
-                    for can in g[c]:
-                        if can == endWord: return lvl + 1
-                        q.append((can, lvl + 1))
+                for w in g[c]:
+                    if w in other_visted:
+                        return lvl + other_visted[w]
+                    if w not in visted:
+                        visted[w] = lvl + 1
+                        q.append((w, lvl +1))
+            return None
+        if endWord not in ws or not endWord or not beginWord or not ws:return 0
+        ans = None
+        q_begin = collections.deque([(beginWord,1)])
+        q_end = collections.deque([(endWord, 1)])
+        beginWord_visted = {beginWord:1}
+        endWord_visted = {endWord:1}
+
+        while q_begin and q_end:
+                
+            ans = helper(q_begin, beginWord_visted, endWord_visted)
+            if ans: 
+                return ans
+            ans = helper(q_end, endWord_visted, beginWord_visted)
+            if ans: 
+                return ans
+
         return 0
-                        
+ 
