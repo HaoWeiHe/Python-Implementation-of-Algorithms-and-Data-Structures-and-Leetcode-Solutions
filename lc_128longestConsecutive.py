682abbb86a8676b764e5c013a676d3eea65fa272
HaoWeiHe <altar396@gmail.com>
2021-03-22 14:10:13 +0800

    opt by remove set, use compare if same as previous value instead. (if its equal to the previous value, just continue

--- lc_128longestConsecutive.py
+++ lc_128longestConsecutive.py
@@ -10,6 +10,34 @@ class Solution(object):
         
     
         """
+        if not nums:return 0
+       
+        nums.sort() #nlogn
+        tmp, ans = 1,1
+        for idx, e in enumerate(nums):
+            if idx ==0:
+                continue
+            if e == nums[idx-1]:
+                continue
+            elif e == nums[idx-1] + 1:
+                tmp +=1
+            else:
+                tmp = 1
+      
+            ans = max(ans, tmp)
+        return ans
+        
+    def longestConsecutive2(self, nums):
+        """
+       [100,4,200,1,3,2]
+       [1,2,3,4,100,200]
+    tmp 1 
+    ans 1 2  3 4 1  2
+        
+        [0,3,7,2,5,8,4,6,0,1]
+        
+    
+        """
         nums = list(set(nums))
         nums.sort() #nlogn
         tmp, ans = 1,0
