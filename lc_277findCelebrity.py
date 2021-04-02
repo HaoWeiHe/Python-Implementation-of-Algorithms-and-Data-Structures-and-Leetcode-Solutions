520bfb86d5b98c3867fa5b025f64a123662d0666
HaoWeiHe <altar396@gmail.com>
2021-04-02 21:51:59 +0800

    opt time complexity to O(n)

--- lc_277findCelebrity.py
+++ lc_277findCelebrity.py
@@ -10,6 +10,22 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        candidate = 0 
+        for i in range(1,n):
+            if knows(candidate,i):
+                candidate = i
+        
+        for i in range(n):
+            if i == candidate:
+                continue
+            if knows(candidate,i) or not knows(i,candidate):
+                return -1
+        return candidate
+    def findCelebrity2(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         
         
         for b in range(n):
