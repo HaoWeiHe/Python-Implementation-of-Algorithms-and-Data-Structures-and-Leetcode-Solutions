8391b96a83e34b9a0fd9462678615c588bc8e282
HaoWeiHe <altar396@gmail.com>
2021-04-06 17:24:42 +0800

    return sum(lst) - sum(min_subarry), so the question turn into find out the min_subarry. Here I use sliding windows and find the min_sunarry within O(n)

---
+++ lc_1423maxScore.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def maxScore(self, lst, k):
+        """
+        [1,2,3,4,5,6,1],
+         i     j
+         
+ tmp 6
+ mn = 6
+ 
+        [1,79,80,1,1,1,200,1]
+         i         j
+            i         j
+                i         j
+                  i         j
+         
+         remove the window size of n-k subarry 
+         return sum - sum((n:k+1))
+         
+         -> max n_k subarry
+        """
+        if not lst:
+            return 0
+        if k >= len(lst):
+            return sum(lst)
+        
+        presum = [lst[0]]
+        for i in range(1, len(lst)):
+            presum.append(presum[i-1] +lst[i])
+        
+        i,j = 0, len(lst) - k -1
+        
+        mn = float("inf")
+        while j < len(lst):
+            if i ==0:
+                mn = min(mn,presum[j])
+            else:
+                mn = min(mn, presum[j] - presum[i-1])
+            i +=1 
+            j +=1 
+            
+        return sum(lst) - mn
