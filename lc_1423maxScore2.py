19d1aa2bd4ec597ca6a5dabbf350f6e6c96fe406
HaoWeiHe <altar396@gmail.com>
2021-04-12 17:19:09 +0800

    sum_all - min(sublist whose lenght == n - k ) where n = len(cardPoints)

--- lc_1423maxScore2.py
+++ lc_1423maxScore2.py
@@ -2,18 +2,42 @@ class Solution(object):
     def maxScore(self, cardPoints, k):
         """
         [1,2,3,4,5,6,1] 
-        rt, lf
-        (0,k)
-        (1,k-1)
-        (2, k-2)
-        .
-        (k, 0)
-          
+        retain = 
+        sum_all - min(sublist whose lenght == n - k ) where n = len(cardPoints)
+        pre = (0~ (n-k))
         """
-        res = 0
-        for i in range(k+1):
-            pre = sum(cardPoints[:i])
-            post = sum(cardPoints[-(k-i):]) if i!=k else 0
-            
-            res = max(res,pre + post )
-        return res
+        if k >= len(cardPoints):
+            return sum(cardPoints)
+        n = len(cardPoints)
+        sublst_number = n-k #[100,40,17,9,73,75] n = 6, k = 3
+        
+        cur = sum(cardPoints[:sublst_number])
+        
+        mn = cur
+
+        for i in range(sublst_number, n):
+             
+            cur = cur -  cardPoints[i-sublst_number] + cardPoints[i]
+            mn = min(mn,cur )
+        return sum(cardPoints) - mn
+    def maxScore2(self, cardPoints, k):
+        """
+        [1,2,3,4,5,6,1] 
+        retain = 
+        sum_all - min(sublist whose lenght == n - k ) where n = len(cardPoints)
+        pre = (0~ (n-k))
+        """
+        if k <= len(cardPoints):
+            return sum(cardPoints)
+        n = len(cardPoints)
+        tmp = cardPoints[:n-k]
+        mn = tmp
+        for i in range(n-k, n):
+            mn = min(tmp, tmp - cardPoints[i-k] + cardPoints[i])
+        return sum(cardPoints) - tmp
+
+
+sol = Solution()
+cardPoints, k = [100,40,17,9,73,75], 3
+print(cardPoints)
+print(sol.maxScore(cardPoints, k ))
