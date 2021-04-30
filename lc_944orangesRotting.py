2ec3da4887fbc3ced05b9e2abf977b652d5369fe
HaoWeiHe <altar396@gmail.com>
2021-04-30 23:06:49 +0800

    optimize with change the status of orange right away when we put orange into queue and create a fresh indicator to compute the number of fresh orange that have not turn to rotten

--- lc_944orangesRotting.py
+++ lc_944orangesRotting.py
@@ -7,11 +7,14 @@ class Solution(object):
         once finish, check if all orange tern to rotten
         """
         q = deque([])
+        fresh = 0 
         m,n = len(g), len(g[0])
         for i in range(m):
             for j in range(n):
                 if g[i][j] == 2:
                     q.append((i,j))
+                elif g[i][j] == 1:
+                    fresh += 1
         ans = 0
         while q:
             ans += 1
@@ -19,13 +22,12 @@ class Solution(object):
             for _ in range(len(q)):
                 
                 top = q.popleft()
-                g[top[0]][top[1]] = 2
+                
                 for dx, dy in [[0,1],[0,-1],[1,0],[-1,0]]:
                     x,y = dx +top[0], dy + top[1]
-                    if 0<=x < m and 0 <= y < n and g[x][y]==1 and (x,y) not in q:
+                    if 0<=x < m and 0 <= y < n and g[x][y]==1:
+                        g[x][y] = 2
                         q.append((x,y))
-   
-        for i in range(m):
-            for j in range(n):
-                if g[i][j] ==1:return -1
-        return max(0,ans-1)
+                        fresh -=1
+       
+        return max(0,ans-1) if fresh ==0 else -1
