78a41c5f225e8bbef3a469294864562bf850c3d5
HaoWeiHe <altar396@gmail.com>
2021-05-16 21:46:22 +0800

    puring earlier

--- lc_1631minimumEffortPath.py
+++ lc_1631minimumEffortPath.py
@@ -1,14 +1,16 @@
+
 class Solution(object):
 	def minimumEffortPath(self, heights):
 
 		m,n = len(heights), len(heights[0])
 		def bfs(cost):
 			q = deque([(0,0)])
-
+			
 			seen = set()
+			
 			while q:
 				x, y = q.popleft()
-				seen.add((x,y))
+				# seen.add((x,y))
 				if x == m-1 and y == n-1:
 					return True
 				for dx, dy in [[0,1],[0,-1],[1,0],[-1,0]]:
@@ -17,14 +19,16 @@ class Solution(object):
 						continue
 					if (nx,ny) in seen:
 						continue
-					seen.add((nx,ny))
 					if abs(heights[x][y] - heights[nx][ny]) > cost:
 						continue
+					seen.add((nx,ny))
+					
 					q.append((nx,ny))
+			return False
 
 		l,r = 0, 1000000
 		while l < r:
-			mid = l + (r-l)/2
+			mid =l + (r-l)/2
 			
 			if bfs(mid):
 				r = mid
