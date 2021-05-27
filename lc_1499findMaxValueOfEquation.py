ca636d419046776faed5bb7fa9a031afb4c1e73f
HaoWeiHe <altar396@gmail.com>
2021-05-27 16:39:29 +0800

    max heap

--- lc_1499findMaxValueOfEquation.py
+++ lc_1499findMaxValueOfEquation.py
@@ -1,5 +1,31 @@
+from  heapq import heappop as pop, heappush as push
+
+
 class Solution(object):
-	def findMaxValueOfEquation(self, points, k):
+    def findMaxValueOfEquation(self, points, k):
+        """
+        compute yi + yj + xj - xi = (xj + yj) + yi - xi
+        and xj - xi <= k 
+        store: xi as key and yi-xi as value
+         [[1,3],[2,0],[5,10],[6,-10]]
+ 
+        ele    | max_heap           |   ans
+        (1,3)    [(2,1) ]               -inf
+        (2,0)   [(2,2),(2,1)]              4
+        (5,10)  [(5,5)]                    4  
+        """
+        h = []
+        ans = float('-inf')
+        for e in points:
+            xj, yj = e
+            while h and xj - h[0][1] > k:
+                pop(h)
+            if h:
+                ans = max(ans, -1 * h[0][0] + xj + yj)
+            push(h,(-1*(yj - xj), xj))
+        return ans
+            
+	def findMaxValueOfEquation2(self, points, k):
 		"""
 		:type points: List[List[int]]
 		:type k: int
@@ -11,7 +37,7 @@ class Solution(object):
 			i = j -1
 			while i >= 0 and abs(points[i][0] - points[j][0]) <= k:
 				ans = max(ans, abs(points[i][0] - points[j][0]) + points[i][1] + points[j][1] )
-				print(ans, points[i], points[j])
+				
 				i -=1 
 			j += 1
 		return ans
