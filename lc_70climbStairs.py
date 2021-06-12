9c30f2fee3ba60cfc04a14bd330ad0add7e7bdcd
HaoWeiHe <altar396@gmail.com>
2021-06-12 17:41:26 +0800

    dp with recursive sol

--- lc_70climbStairs.py
+++ lc_70climbStairs.py
@@ -11,6 +11,28 @@ class Solution(object):
         n == 2: 2
         
         """
+        mem = [0]*(n+1)
+        
+        def dfs(n):
+            if n < 3:
+                return n
+            if mem[n]!= 0:
+                return mem[n]
+            mem[n] =  dfs(n-1) + dfs(n-2)    
+            return mem[n]
+        return dfs(n)
+    def climbStairs2(self, n):
+        """
+        dp[i] = dp[i-1] + dp[i-2]
+        cur = prev1 + prev_2
+        prev1 = cur
+        prev2 = prev1
+        
+        n == 0 : 0
+        n == 1: 1
+        n == 2: 2
+        
+        """
         if n < 3:
             return n
         cur, prev1, prev2 = 0,2,1
