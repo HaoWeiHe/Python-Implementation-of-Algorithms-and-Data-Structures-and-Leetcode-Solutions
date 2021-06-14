4cafafde5b940fadd2f565d56ac86539030b92b9
HaoWeiHe <altar396@gmail.com>
2021-06-14 17:07:21 +0800

    bs

---
+++ lc_367.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        16:
+        1 2 4
+        1 4 16 bingo
+        
+        14
+        1 2 4
+        1 4 16
+        target = 16
+        1,16 49
+        1,6  3
+        3~6 
+        
+        """
+        
+        low, high = 1, num
+        
+        def bs(low, high):
+            if low > high:
+                return False
+            mid = (low + high) / 2
+            if mid* mid == num:
+                return mid
+            
+            if mid* mid < num:
+                return bs(mid+1, num)
+            return bs(low, mid -1 )
+        return bs(0, num)
