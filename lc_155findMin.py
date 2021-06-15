23f1975ea927e28d29ad6c7ca61cca18a72979cf
HaoWeiHe <altar396@gmail.com>
2021-06-15 23:05:33 +0800

    bs sol

--- lc_155findMin.py
+++ lc_155findMin.py
@@ -1,3 +1,17 @@
+
+class Solution(object):
+    def findMin(self, nums):
+        if nums[0] < nums[len(nums) -1 ]:
+            return nums[0]
+        l, r = 0, len(nums)
+        while l < r:
+            m = l + (r-l)/2
+            if nums[mid] >= nums[0]:
+                l = mid + 1
+            else:
+                r = m 
+        return nums[l]
+
 class Solution(object):
     def findMin(self, nums):
         """
