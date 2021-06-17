144be4381c12795d68ab0cf9b4b805f1f3bc991c
HaoWeiHe <altar396@gmail.com>
2021-06-17 15:22:41 +0800

    easy understand recursive reversion

--- alg_mergeSort.py
+++ alg_mergeSort.py
@@ -1,4 +1,42 @@
-def Merge_Sort(array):
+"""
+        538462714
+     5384   62714
+    53  84
+  5  3
+
+  35 
+"""
+
+def mergeSort(arr):
+
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr)/2
+
+    left_arr = mergeSort(arr[:mid])
+    right_arr  = mergeSort(arr[mid:])
+
+    i, j = 0, 0 
+    ans = []
+    while i < len(left_arr) and j < len(right_arr):
+        if left_arr[i] <= right_arr[j]:
+            ans.append(left_arr[i])
+            i += 1
+        else:
+            ans.append(right_arr[j])
+            j += 1
+
+    if i != len(left_arr):
+        ans += left_arr[i:] 
+
+    if j != len(right_arr):
+        ans += right_arr[j:]
+    return ans
+
+
+
+def Merge_Sort2(array):
     if len(array) > 1:
         mid = len(array) // 2
         left_array = array[:mid]
@@ -30,6 +68,3 @@ def Merge_Sort(array):
             left_index = left_index + 1
             merged_index = merged_index + 1
 ​
-Numbers = [41, 33, 17, 80, 61, 5, 55]
-Merge_Sort(Numbers)
-print(Numbers)
