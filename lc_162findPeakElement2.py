e215ae71c1988bfb3ecc09f3e004510c7e5a2733
HaoWeiHe <altar396@gmail.com>
2021-06-18 23:46:56 +0800

    opt

--- lc_162findPeakElement2.py
+++ lc_162findPeakElement2.py
@@ -12,11 +12,11 @@ class Solution(object):
         if len(nums) <=1:
             return 0
         
-        l,r = 0 , len(nums)  #0,1, 0,2
+        l,r = 0 , len(nums) - 1
         while l < r:
-            m = l + (r-l)/2  #m = 1
-            if nums[m] >= nums[m+1]:
-                r = m # r= 1
+            m = l + (r-l)/2  
+            if nums[m] > nums[m+1]:
+                r = m
             else:
                 l = m + 1
         return l
