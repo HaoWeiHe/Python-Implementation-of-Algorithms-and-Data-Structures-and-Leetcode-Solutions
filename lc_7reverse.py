bba98d4504e35bbf75193b901e9871ff43ec57c8
HaoWeiHe <altar396@gmail.com>
2021-06-19 20:36:58 +0800

    avoid overfitting

--- lc_7reverse.py
+++ lc_7reverse.py
@@ -1,20 +1,66 @@
 class Solution(object):
     def reverse(self, x):
         """
-        12300
-          ^
-
-        """
-        sign = [1,-1][x < 0 ]
-        x = str(abs(x))
-        detet = 0 
-        for i in range(len(x)-1, -1, -1):
-            if x[i]  != 0:
-                detet = i 
-                break
-        ans =  sign * int(x[:detet+1][::-1]) 
-        return ans if -1*2**31  < ans < 2**31 -1 else 0
+        123
+         ^
+        tmp 3   2 1 
+        res 12  1 0
         
+        ans = 0
+        ans *10 + 3 = 3
+        32
+        
+        """
+        mx =  2**31 -1 
+        
+        sign = [-1,1][x > 0]
+
+        x = abs(x)
+        res = 0 
+
+        while x:
+            mod = x % 10
+            if sign == 1:
+                if res > mx/ 10 or (res == mx/10 and mod > 7):
+                    return 0
+            else:
+                if res > mx/ 10 or (res == mx/10 and mod > 8):
+                    return 0
+            res = res*10 + mod
+            x /= 10
+        return sign* res 
+class Solution(object):
+    def reverse(self, x):
+        """
+        123
+         ^
+        tmp 3   2 1 
+        res 12  1 0
+        
+        ans = 0
+        ans *10 + 3 = 3
+        32
+        
+        """
+        mx =  2**31 -1 
+        
+        sign = [-1,1][x > 0]
+
+        x = abs(x)
+        res = 0 
+
+        while x:
+            mod = x % 10
+            if sign == 1:
+                if res > mx/ 10 or (res == mx/10 and mod > 7):
+                    return 0
+            else:
+                if res > mx/ 10 or (res == mx/10 and mod > 8):
+                    return 0
+            res = res*10 + mod
+            x /= 10
+        return sign* res 
+
     def reverse2(self, x):
         """
         123
@@ -31,6 +77,7 @@ class Solution(object):
         
         if x < -1*2**31 or x >= mx:
             return 0
+
         neg = x < 0
         x = abs(x)
         res = 0 
