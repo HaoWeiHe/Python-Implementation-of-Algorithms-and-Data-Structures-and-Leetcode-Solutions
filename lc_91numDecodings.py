4a69837c91697732dab105452988f295febefad0
HaoWeiHe <altar396@gmail.com>
2021-06-20 23:24:53 +0800

    slice version(slower..) just practice

--- lc_91numDecodings.py
+++ lc_91numDecodings.py
@@ -1,7 +1,46 @@
 
 
+
+
 class Solution(object):
     def numDecodings(self, s):
+        """
+         "12"
+          -> 1 + f(2) if 1 in s
+          -> 12 + f(""), if 12 in s,  return, counter + 1
+        hashtable = {1:A, 2:B, 3:..}
+        
+        2326
+      [2]326   [23]26
+ 2[3]26  2[32]6 23[2]6
+        
+        
+        """
+        h = {str(no - 64): chr(no) for no in range(65,91)}
+        mem = {}
+        #226
+        #012
+        def dfs(s):
+            ans = 0 
+
+            if s == "": 
+                return 1
+            
+            if s in mem:
+                return mem[s]
+            
+            if s[:2] in h and len(s)>1: 
+                ans += dfs(s[2:])
+
+            if s[:1] in h:
+                ans += dfs(s[1:])
+            
+            mem[s] = ans
+            return ans
+
+        return dfs(s)
+
+    def numDecodings2(self, s):
         """
          "12"
           -> 1 + f(2) if 1 in s
