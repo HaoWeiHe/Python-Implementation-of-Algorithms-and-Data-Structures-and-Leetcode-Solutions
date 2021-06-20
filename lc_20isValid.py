e7c6cd9f5827f987e449325165448eed1bfaf6b9
HaoWeiHe <altar396@gmail.com>
2021-06-20 10:51:21 +0800

    use stack to check if paris or not

---
+++ lc_20isValid.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+         "()"
+         s = [(]
+         if ):
+            cmp if top of s is the pair of ) 
+        use hash! 
+        """
+        stk = []
+        pairs = {")":"(", "}":"{", "]":"["}
+        for e in s:
+            if e in ["(", "{","["]:
+                stk.append(e)
+            else:
+                if not stk:
+                    return False
+                if pairs[e] != stk.pop():
+                    return False
+        return True if not stk else False
+                
