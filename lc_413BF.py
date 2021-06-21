b734ace2ddbffadb3237831c687cac63e0dd8076
HaoWeiHe <altar396@gmail.com>
2021-06-21 16:14:27 +0800

    for loop

---
+++ lc_413BF.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        for i in range(1,n + 1):
+            
+        """
+        ans = []
+        for i in range(1,n + 1):
+            if i%3 == 0 and i%5 ==0:
+                ans.append("FizzBuzz")
+            elif i%3 ==0:
+                ans.append("Fizz")
+            elif i%5 ==0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+        return ans
+            
+        
