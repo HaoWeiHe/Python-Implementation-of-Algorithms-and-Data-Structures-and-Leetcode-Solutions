6020a6df7a0788550600b6b2fa6ff0385d5bbe62
HaoWeiHe <altar396@gmail.com>
2021-06-28 16:26:25 +0800

    use O(n) time complexity

--- lc_1911maxAlternatingSum.py
+++ lc_1911maxAlternatingSum.py
@@ -1,6 +1,24 @@
 class Solution(object):
     def maxAlternatingSum(self, nums):
         """
+         can I always put 0 here: [+4,-2,+5,-3,-0]
+                                            51
+         0 3 5 2 4
+          3 2   2
+          
+        [5,6,7,8,0]
+        0 8 7 6 5
+         8    
+         
+        """
+        nums = nums+[0]
+        res = 0 
+        for idx in range(1, len(nums)):
+            if nums[idx] < nums[idx - 1]:
+                res += (nums[idx-1] - nums[idx])
+        return res
+    def maxAlternatingSum2(self, nums):
+        """
         :type nums: List[int]
         :rtype: int
         """
