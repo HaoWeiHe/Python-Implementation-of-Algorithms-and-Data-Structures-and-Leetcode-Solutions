6b30756d24669c9144e64e6ce9ee68c1dfd3d10f
HaoWeiHe <altar396@gmail.com>
2021-07-01 20:05:30 +0800

    opt

--- lc_53maxSubArray.py
+++ lc_53maxSubArray.py
@@ -2,6 +2,23 @@ class Solution(object):
     def maxSubArray(self, nums):
         """
         [-2,1,-3,4,-1,2,1,-5,4]
+                           v
+tmp      0  1 0  4 3  5 6  1 5
+ans      -2 1 1  4 4  5 6  6 6
+        """
+        if not nums:
+            return 0
+        
+        tmp, ans = 0, nums[0]
+        
+        for e in nums:
+            tmp += e
+            ans = max(ans, tmp)
+            tmp = max(0,tmp)
+        return ans
+    def maxSubArray2(self, nums):
+        """
+        [-2,1,-3,4,-1,2,1,-5,4]
     now  -2 1 -2 4  3 5 6
     res  -2 1  1 4  4 5 6
         """
