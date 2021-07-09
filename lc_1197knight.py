d8df1a4a764cfe090cb0e46cd647784370b1f274
HaoWeiHe <altar396@gmail.com>
2021-07-09 16:18:24 +0800

    use bfs /w v

---
+++ lc_1197knight.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def minKnightMoves(self, x, y):
+        """
+        [0,0] (x2,x1) or (x1,x2)
+        [2,1]
+        
+        [0,0]
+        [2,1]
+        [4,2]
+        [3,4]
+        1. abs(x,y)
+        [4,2] -> (2, 1) or (-2, 1) or (1,2) or -1,2
+        """
+        x, y = abs(x), abs(y)
+        q = deque([(0,0,0)])
+        v = set()
+        while q:
+            i,j, level = q.popleft()
+
+            if (i,j) == (x,y):
+                return level
+           
+            
+            for dx, dy in [(2,1), (2,-1),(-2,1), (1,2), (-1,2),(1,-2)]:
+                newx, newy = dx+i, dy +j
+                if (newx, newy) in v:
+                    continue
+                v.add((newx, newy))
+                q.append((newx, newy, level + 1))
+                
+                
