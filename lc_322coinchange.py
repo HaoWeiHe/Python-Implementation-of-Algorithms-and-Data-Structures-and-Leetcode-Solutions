c96ff0158e2237dc548283015ef08246e7f8e0aa
HaoWeiHe <haoweihohoho@gmail.com>
2021-07-15 17:26:36 +0800

    with meme

--- lc_322coinchange.py
+++ lc_322coinchange.py
@@ -1,6 +1,46 @@
 class Solution(object):
     def coinChange(self, coins, amount):
         """
+        1,2,5 return 1
+        < 0 return inf
+        3 -> 1,2
+        5 -> 0+5, 2+3, 1+4
+        mem[]
+        4 = 2+2, 1+3
+        check 2: 1
+        1,2,5
+        7 -> 5+2, 2+5, 1 + 6
+        """
+        self.mem = {}
+        
+        def dfs(rest):
+            if rest == 0:
+                return 0,1 
+            if rest < 0 :
+                return 0, 0
+            
+            if rest in coins:
+                self.mem[rest] = 1
+                return 1, 1
+            
+            if rest in self.mem:
+                return self.mem[rest], 1
+
+            ans = float('inf')
+            for e in coins :
+                v, work = dfs(rest -e)
+                if work:
+                    ans = min(1 +v ,ans)
+            self.mem[rest] = ans
+            
+            return self.mem[rest],  ans != float('inf')
+        
+        rest, work = dfs(amount)
+        
+        return rest if work else -1
+
+    def coinChange2(self, coins, amount):
+        """
         :type coins: List[int]
         :type amount: int
         :rtype: int
