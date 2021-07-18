131df44ecddd9fc5a16914999cd88eeb8741782d
HaoWeiHe <haoweihohoho@gmail.com>
2021-07-18 19:44:49 +0800

    opt time complexity to O(n)

--- lc_244WordDistance.py
+++ lc_244WordDistance.py
@@ -7,6 +7,12 @@ class WordDistance(object):
         makes:[1,4]
         perfect:2
         coding:3
+        
+        [3,6,8,12]
+             ^
+        [2,7,9]
+             ^
+    ans 1  1
         """
         self.d = defaultdict(list)
         for idx, w in enumerate(wordsDict):
@@ -22,10 +28,19 @@ class WordDistance(object):
         if word1 not in self.d or word2 not in self.d:
             return 
         ans = float('inf')
-        for i in self.d[word1]:
-            for j in self.d[word2]:
-                ans = min(ans, abs(i-j))
+        i1, i2= 0 ,0
+        loc1, loc2 = self.d[word1], self.d[word2]
+        while i1 < len(loc1) and i2 < len(loc2):
+            ans = min(ans, abs(loc1[i1] - loc2[i2]))
+            if loc1[i1] < loc2[i2]:
+                i1 += 1
+            else:
+                i2 += 1
         return ans
+                # for i in self.d[word1]:
+        #     for j in self.d[word2]:
+        #         ans = min(ans, abs(i-j))
+        # return ans
 
 
 # Your WordDistance object will be instantiated and called as such:
