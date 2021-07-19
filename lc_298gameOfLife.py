fa303a3e136acd18ebed5e2722d6458bb34b57fb
HaoWeiHe <haoweihohoho@gmail.com>
2021-07-19 19:34:46 +0800

    opt /w space complexity

--- lc_298gameOfLife.py
+++ lc_298gameOfLife.py
@@ -1,4 +1,36 @@
 class Solution(object):
+    def gameOfLife2(self, board):
+        """
+         live: 
+             <2 die
+             ==2 or 3 live
+             >3 dies, 
+         die:
+          ==3 live -> live
+        """
+        m,n = len(board), len(board[0])
+        
+        for i in range(m):
+            for j in range(n):
+                live = 0
+                for dx, dy in [[0,1],[0,-1], [1,0], [-1,0], [1,1], [1,-1],[-1,1],[-1,-1]]:
+                    nx, ny = dx + i, dy + j 
+                    if not (0 <= nx < m and 0 <= ny < n):
+                        continue
+                    if board[nx][ny] == 1 or board[nx][ny]== 3 :
+                        live +=1
+                if board[i][j] == 0 and live ==3:
+                    board[i][j] = 2
+                if board[i][j] == 1 and (live < 2 or live > 3):
+                    board[i][j] = 3
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 2:
+                    board[i][j] = 1
+                elif board[i][j] == 3:
+                    board[i][j] = 0
+        return board 
+        
     def gameOfLife(self, board):
         """
          live: 
