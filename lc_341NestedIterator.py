6864660358ccdcbb476fba9130984fb61c7b0215
HaoWeiHe <haoweihohoho@gmail.com>
2021-07-22 20:16:44 +0800

    use dfs

---
+++ lc_341NestedIterator.py
@@ -0,0 +1,92 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+# """
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        [[1,1],2,[1,1]]
+         [dfs(e) + 2 + dfs(e) for e in Lst]
+        """
+        # self.lst = nestedList
+        def dfs(r):
+            ans = []
+            for e in r:
+                if e.isInteger():
+                    ans.append(e.getInteger())
+                else:
+                    ans += dfs(e.getList())
+            return ans
+        
+        self.lst = dfs(nestedList)
+        self.idx = -1
+        
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        self.idx += 1
+        return self.lst[self.idx]
+        
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        if self.idx == len(self.lst)-1:
+            return False
+        return True
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
