3428ae6e544e03a2a993a2a5f43b26a407e35f7f
HaoWeiHe <haoweihohoho@gmail.com>
2021-08-04 23:20:02 +0800

    opt code

--- lc_343integerBreak.py
+++ lc_343integerBreak.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        dp = [1] *(n+1)
+        dp = [i for i in range(n+1)]
         if n == 2:
             return 1
         if n ==3:
             return 2
         if n ==4:
             return 4
-        for i in range(2,5):
-            dp[i] = i
+
         for i in range(2, n+1):
             for j in range(2, i/2 + 1):
                 
