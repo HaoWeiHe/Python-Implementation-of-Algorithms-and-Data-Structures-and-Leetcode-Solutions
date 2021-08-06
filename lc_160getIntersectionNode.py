50ececc6a5cfcce3f5bf28f4bcca8b26f8c1fa4a
HaoWeiHe <haoweihohoho@gmail.com>
2021-08-06 13:25:26 +0800

    O(1)

--- lc_160getIntersectionNode.py
+++ lc_160getIntersectionNode.py
@@ -5,43 +5,13 @@
 #         self.next = None
 
 class Solution(object):
-    def getIntersectionNode(self, headA, headB):
-        """
-        head A-> headB
-        headB-> headA
-        
-        a + c + b = b + c + a
-        """
-        Apre, Bpre = ListNode(), ListNode()
-  
-        p1, p2 = headA, headB
-        flag = 0
-        while p1 != p2 and flag <2:
-           
-            if p1 and p1.next:
-                p1 = p1.next
-            else:
-                p1 = headB
-                flag += 1
-                
-            if p2 and p2.next:
-                p2 = p2.next
-            else:
-                p2 = headA
-            
-        
-        return p1 if flag<2 else None
-    def getIntersectionNode2(self, headA, headB):
+    def getIntersectionNode(self, a, b):
         """
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        a = set()
-        while headA:
-            a.add(headA)
-            headA = headA.next
-        
-        while headB:
-            if headB in a: return headB
-            headB = headB.next
-            
+        p1, p2 = a,b
+        while p1 != p2:
+            p1 = p1.next if p1 else b
+            p2 = p2.next if p2 else a
+        return p1
