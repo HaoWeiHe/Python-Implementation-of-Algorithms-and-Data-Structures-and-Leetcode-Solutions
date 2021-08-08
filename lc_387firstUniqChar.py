3b6aaad885bd146f7e2226d40d459d61198cd98e
HaoWeiHe <haoweihohoho@gmail.com>
2021-08-08 21:51:03 +0800

    opt space complex, from O(n) to O(set(n))

--- lc_387firstUniqChar.py
+++ lc_387firstUniqChar.py
@@ -1,4 +1,24 @@
 class Solution(object):
+    def firstUniqChar2(self, s):
+        """
+        {l:0, e:inf, t:3, c:4, o:5, d:6}
+        {l:inf,o:9,v:2,e:inf, t: 3, 6:7, c:8, d:10}
+        
+        leetcode
+        
+        
+        """
+        d = {}
+        for i, v in enumerate(s):
+            if v in d:
+                d[v] = 'inf'
+            else:
+                d[v] = i
+        
+        for i, v in enumerate(s):
+            if i == d[v]:
+                return i
+        return -1
     def firstUniqChar(self, s):
         """
         {l:0, e:1, t:3, c:4, o:5, d:6}
