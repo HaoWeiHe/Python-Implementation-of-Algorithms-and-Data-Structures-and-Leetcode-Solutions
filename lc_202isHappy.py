5fd9916903901f15d51a64442361a201ef155e10
HaoWeiHe <haoweihohoho@gmail.com>
2021-08-31 22:27:11 -0700

    use slow/ fast pointer

--- lc_202isHappy.py
+++ lc_202isHappy.py
@@ -12,10 +12,30 @@ class Solution(object):
                 tmp += r**2
                 n = q
             return tmp
-                
-        while n and n not in h:
-            h.add(n)
-            n = get_next(n)
+        fast, slow = get_next(n) ,n
+        while fast != 1 and fast!= slow:
+            fast = get_next(get_next(fast))
+            slow = get_next(slow)
             
            
-        return n == 1 
+        return fast == 1 
+    def isHappy2(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        h = set()
+        def get_next(n):
+            tmp =  0
+            while n:
+                q,r = divmod(n,10)
+                tmp += r**2
+                n = q
+            return tmp
+        fast, slow = n, get_next(n)   
+        while fast != 1 and fast!= slow:
+            fast = get_next(get_next(fast))
+            slow = get_next(slow)
+            
+           
+        return fast == 1 
