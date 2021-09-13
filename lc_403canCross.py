a879d03fb7a2183bc594e69aea6954194edd1255
HaoWeiHe <haoweihohoho@gmail.com>
2021-09-12 22:36:17 -0700

    dp solution. h = {stones_idx:[set_of_all_possible_k_value]}

--- lc_403canCross.py
+++ lc_403canCross.py
@@ -1,6 +1,28 @@
 class Solution(object):
     def canCross(self, stones):
         """
+        map[idex] = (k1, k2,k3)
+        [0,1,3,5,6,8,12,17]
+         1
+         h = {0:[1],1:[],3:[],5:[]}
+         h = {e:defaultdict(list) for e in stones}
+         
+        """
+
+        h = {e:set() for e in stones}
+        h[0].add(0)
+     
+        for e in stones:
+            for k in h[e]:
+                for plus in range(-1,2):
+                    if plus+k <= 0 or (e + k + plus) not in h:
+                        continue
+                    h[e + k + plus].add(k+plus)
+        
+        return len(h[stones[-1]]) > 0 
+        
+    def canCross2(self, stones):
+        """
         :type stones: List[int]
         :rtype: bool
         """
