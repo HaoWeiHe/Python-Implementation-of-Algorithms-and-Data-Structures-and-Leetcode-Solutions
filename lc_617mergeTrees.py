fa93dca782163683b9ec1da17e03d4fa3a328f9e
HaoWeiHe <haoweihohoho@gmail.com>
2021-09-14 17:51:49 -0700

    opt

--- lc_617mergeTrees.py
+++ lc_617mergeTrees.py
@@ -5,6 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    def mergeTrees2(self, r1, r2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        """
+        if not r1 and not r2:
+            return 
+        
+        root = TreeNode((r1.val if r1 else 0 ) + (r2.val if r2 else 0))
+ 
+      
+            
+        root.left =  self.mergeTrees(r1.left if r1 else None, r2.left if r2 else None)
+        root.right = self.mergeTrees(r1.right if r1 else None, r2.right if r2 else None)
+        return root
+class Solution(object):
     def mergeTrees(self, r1, r2):
         """
         :type root1: TreeNode
