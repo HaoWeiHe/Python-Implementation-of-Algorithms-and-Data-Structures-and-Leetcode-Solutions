46451246780b4e95a8262dfb56e862b7ee02c41d
HaoWeiHe <haoweihohoho@gmail.com>
2021-09-16 21:30:49 -0700

    dfs

---
+++ lc_590postorder.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        
+        res = []
+        for e in root.children:
+            res += self.postorder(e)
+        res += [root.val]
+        return res
+        
