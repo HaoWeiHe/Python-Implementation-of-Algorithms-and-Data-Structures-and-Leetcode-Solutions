6bbb47a7ab213883aa15f13c0c54b4c052df1a01
HaoWeiHe <haoweihohoho@gmail.com>
2021-09-22 17:09:57 -0700

    bfs sol, space O(n), time O(n*nlgn)

---
+++ lc_783minDiffInBST.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minDiffInBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.lst =[]
+        self.ans = float("inf")
+        def bs(val):
+            l, r= 0, len(self.lst)
+            while l < r:
+                m = l + (r-l)/2
+                if self.lst[m] > val:
+                    r = m
+                else:
+                    l = m + 1
+            return l
+                
+        def dfs(root):
+            if not root:return 
+            insert_idx = bs(root.val)
+            self.lst = self.lst[:insert_idx] + [root.val] + self.lst[insert_idx:]
+            
+            if insert_idx - 1 >= 0 :
+                self.ans = min(self.ans, abs(root.val- self.lst[insert_idx - 1] ))
+            if insert_idx + 1 < len(self.lst):
+                self.ans = min(self.ans, abs(root.val- self.lst[insert_idx + 1] )) 
+            dfs(root.right) 
+            dfs(root.left)
+        dfs(root)
+      
+        return self.ans
+            
