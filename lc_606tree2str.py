9db1dba47e98bd56364a45c418ab91cccf49f3da
HaoWeiHe <haoweihohoho@gmail.com>
2021-09-24 15:59:43 -0700

    different rules for single left child

---
+++ lc_606tree2str.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def tree2str(self, root):
+        """
+        :type root: TreeNode
+        :rtype: str
+        """
+        self.res = []
+        def dfs(root):
+            if not root:
+                return []
+            if not root.right and not root.left :
+                return [str(root.val)] 
+            if not root.right:
+                return [str(root.val)] + ["("] + dfs(root.left) + [")"] 
+            return [str(root.val)] + ["("] + dfs(root.left) + [")"] + ["("] + dfs(root.right) + [")"] 
+        return "".join(dfs(root))
