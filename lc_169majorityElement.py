b594b2c8a324edbdb17488d82a910ba489cf517a
HaoWeiHe <haoweihohoho@gmail.com>
2021-11-01 23:58:02 -0700

    Counter

---
+++ lc_169majorityElement.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+      
+        return Counter(nums).most_common(1)[0][0]
