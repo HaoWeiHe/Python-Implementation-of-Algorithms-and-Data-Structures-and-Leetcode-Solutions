eac9a9e7a1988d83dd208eb7eb0e40d796ffe553
HaoWeiHe <haoweihohoho@gmail.com>
2021-11-24 17:16:24 -0800

    use hashmap to reduce the range O(n) -> O(n/2)

--- lc_710 Blacklist.py
+++ lc_710 Blacklist.py
@@ -1,21 +1,34 @@
-from random import randrange
 class Solution(object):
 
-    def __init__(self, n, blacklist):
+    def __init__(self, n, lst):
         """
-        :type n: int
-        :type blacklist: List[int]
+        [0,1,2,3,4,5,6,7]
+                 ^
+                   
+            x     4-x
+        if block = [0,1,2,4]
+        {0:5} {1:6}
+        
+         
         """
-        self.n = n
-        self.s = [v for v in range(n) if v not in set(blacklist)]
+        lst.sort()
+        self.d, self.lgth = {}, n - len(lst)
+        idx = 0 
+        self.s = set(lst)
+        for ele in range(self.lgth, n):
+            if ele not in self.s:
+                self.d[lst[idx]] = ele
+                idx += 1
 
     def pick(self):
         """
         :rtype: int
         """
-        return random.choice(self.s)
-     
-
+        picked = random.randint(0, self.lgth-1)
+        return self.d[picked] if picked in self.d else picked
+        
+        
+    
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(n, blacklist)
