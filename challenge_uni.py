2b5641b07efa36aa7193d7adb37ced7a6eb8e80f
HaoWeiHe <haoweihohoho@gmail.com>
2021-12-10 23:03:41 -0800

    fix circle issue

--- challenge_uni.py
+++ challenge_uni.py
@@ -1,17 +1,20 @@
 import collections
 class unifind():
 	def __init__(self, lst):
-		self.parent = [e for e in lst]
+		self.parent = [i for i in range(len(lst))]
 		C = collections.Counter(lst)
 		self.rank = [C[ele] for ele in range(len(lst))]
+
 	def find(self,x):
 		if x!= self.parent[x]:
 			self.parent[x] = self.find(self.parent[x])
 		return self.parent[x]
+
 	def union(self,x,y):
-		px, py = self.find(x), self.find(y)
+
+		px, py  = self.find(x), self.find(y)
 		if px == py:
-			return 
+			return True
 		if self.rank[px] < self.rank[py]:
 			self.rank[py] += 1
 			self.parent[px] = py
@@ -19,14 +22,22 @@ class unifind():
 			self.rank[px] += 1
 			self.parent[py] = px
 		else:
+			#px <- py
 			self.rank[px] += 1
-			self.rank[py] = px
+			self.parent[py] = px
+		return False
 
-lst = [0,0,2]
-uniF = unifind(lst)
-for idx, val in enumerate(lst):
-	uniF.union(idx, val)
+tests = [[1,0], [0,0,2], [0,0,0],[3,0,0,3]]
+class sol():
+	def validTree(self, lst):
+		uniF = unifind(lst)
+		for idx, val in enumerate(lst):
+			if idx == val:
+				continue
+			if uniF.union(val, idx):
+				return False
+		return len(set(uniF.parent)) == 1
 
 
-print(len(set(uniF.parent)) == 1)
-
+for lst in tests:
+	print(sol().validTree(lst))
