9d8a7efa74677ece181b44725d9f9e3ff9a1249c
HaoWeiHe <haoweihohoho@gmail.com>
2021-12-28 10:51:08 -0800

    binary search from min(lst) to max(lst) + 1

---
+++ lc_1482minDays.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def minDays(self, bloomDay, m, k):
+        """
+        :type bloomDay: List[int]
+        :type m: int
+        :type k: int
+        :rtype: int
+        l, r = min(bD), len(bD)
+        f(days) >= m
+        [x, x, x, x, _, x, x]
+               ^
+    acc  1  2  3  1  0  1  2
+               %3 ==0-> b += 1
+        
+    
+    bq = 1  1  1  2
+    
+    in the end, check if l is valid
+    if not -1
+        """
+        if m*k > len(bloomDay):
+            return -1
+        l, r = min(bloomDay), max(bloomDay) + 1
+        """
+        [1,10,3,10,2]
+         x    x    x if <= day
+acc_num  1  0 1  0 1   
+    ans = 1 1 2  0  3 
+        day = 3  ans == m (True)
+
+        """
+        def valid(day):
+            acc_num, ans = 0,0
+            for ele in bloomDay:
+                if  ele > day:
+                    acc_num = 0
+                    continue
+                
+                acc_num += 1
+                if acc_num == k:
+                    ans += 1
+                    acc_num = 0 
+            return ans >= m
+        acc_day = 0 
+        while l < r:
+            
+            mid = (l + r) / 2
+            if valid(mid):
+                r = mid
+            else:
+                l = mid +1
+        
+        return l if 0 <= l <= max(bloomDay) else -1
+        
