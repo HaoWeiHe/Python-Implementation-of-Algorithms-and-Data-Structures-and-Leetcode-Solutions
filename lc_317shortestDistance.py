d6ee48add8e06fab783c2e00a121f02b51d9511d
HaoWeiHe <haoweihohoho@gmail.com>
2022-01-01 22:22:11 -0800

    bfs sol from 0

--- lc_317shortestDistance.py
+++ lc_317shortestDistance.py
@@ -1,5 +1,7 @@
+from collections import deque
 class Solution(object):
-    def shortestDistance(self, grid):
+
+    def shortestDistance2(self, grid):
         """
         [
         [x,x,2,x,1],
@@ -52,3 +54,44 @@ class Solution(object):
                 ans = min(ans, self.steps)
         return ans if ans != float("inf") else -1
     
+
+    def shortestDistance(self, grid):
+        m,n = len(grid), len(grid[0])
+        num_house = 0 
+        g = [[(0,0)] * n for _ in range(m)]
+        
+        def explore(x,y,v):
+            q = deque([[x,y,0]])
+            while  q:
+                x,y, step = q.popleft()
+                
+                if (x,y) in v:
+                    continue
+                
+                v.add((x,y))
+
+                if grid[x][y] == 0:
+                    g[x][y] = (g[x][y][0]+ 1, g[x][y][1]+ step)
+                     
+
+                for dx, dy in [[0,1],[0,-1],[1,0],[-1,0]]:
+                    nx, ny = x + dx, y + dy 
+                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 0 :
+                        q.append((nx,ny, step + 1))
+                        
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    num_house += 1 
+                    explore(i,j,set())
+                    
+    
+
+        ans = float("inf")
+        for i in range(m):
+            for j in range(n):
+                if g[i][j][0] == num_house:
+                    ans = min(ans, g[i][j][1])
+        
+        return -1 if ans == float("inf") else ans
