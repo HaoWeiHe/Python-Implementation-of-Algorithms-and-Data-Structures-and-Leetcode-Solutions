c4b3d75b8d6e6552ae6cce1483e3711c3bdcf86a
HaoWeiHe <haoweihohoho@gmail.com>
2022-01-02 00:06:26 -0800

    opt

--- lc_398findDiagonalOrder.py
+++ lc_398findDiagonalOrder.py
@@ -9,28 +9,64 @@ class Solution(object):
         def getdiag(i,j):
             target = i 
             ans = []
-            while True:
-               
+            
+            while i < m and j >= 0:       
                 ans.append(mat[i][j])
                 i +=1
                 j -= 1
-                if i >=m or j < 0: 
-                    break
+
             return ans
         
         counter = 0 
         i = 0 
-        def _iterate(i,j):
+        def gnerate(i,j):
             if counter %2 == 0:
                 ans.extend(getdiag(i,j)[::-1])
             else:
                 ans.extend(getdiag(i,j))           
+
         for j in range(n):
+            gnerate(0,j)
+            counter += 1
             
-            _iterate(0,j)
-            counter += 1
         for i in range(1,m):
-            _iterate(i,j)
+            gnerate(i,j)
+            counter += 1class Solution(object):
+    def findDiagonalOrder(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[int]
+        """
+        ans = []
+        m,n = len(mat), len(mat[0])
+        def getdiag(i,j):
+            target = i 
+            ans = []
+            
+            while i < m and j >= 0:       
+                ans.append(mat[i][j])
+                i +=1
+                j -= 1
+
+            return ans
+        
+        counter = 0 
+        i = 0 
+        def gnerate(i,j):
+            if counter %2 == 0:
+                ans.extend(getdiag(i,j)[::-1])
+            else:
+                ans.extend(getdiag(i,j))           
+
+        for j in range(n):
+            gnerate(0,j)
             counter += 1
+            
+        for i in range(1,m):
+            gnerate(i,j)
+            counter += 1
+            
+        return ans
+
             
         return ans
