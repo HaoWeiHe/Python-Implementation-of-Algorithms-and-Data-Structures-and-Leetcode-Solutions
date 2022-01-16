65bbdedeed8bad349097a4c023f619a286ea1095
HaoWeiHe <haoweihohoho@gmail.com>
2022-01-15 18:06:17 -0800

    time O(n) + space O(n)

--- lc_516longestPalindromeSubseq.py
+++ lc_516longestPalindromeSubseq.py
@@ -1,24 +1,25 @@
 class Solution:
-
-    # def longestPalindromeSubseq(self, s):
-    #     n = len(s)
-    #     dp_prev, dp = [0] *n, [0] * n 
-    #     for l in range(1, n + 1):
-    #         for i in range( n - l + 1):
-    #             j = i + l - 1
-    #             if i == j:
-    #                 dp[i]= 1
-    #                 continue
-    #             if s[i] == s[j]:
-    #                 dp[j] = dp_prev[j - 1] + 2
-    #             else: 
-    #                 dp[j] = max(dp_prev[j], dp[j - 1])
-    #         dp_prev, dp = dp, dp_prev
-    #     return dp_prev[n-1]
     def longestPalindromeSubseq(self, s):
         n = len(s)
+        dp, dpPrev = [0] * n, [0] * n 
+        for i in range(n - 1, -1, -1):
+            dp[i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[j] = dpPrev[j - 1] + 2 
+                else:
+                    dp[j] = max(dpPrev[j], dp[j - 1])
+            
+            dpPrev = dp
+            dp = [0] * n 
+            
+
+        return dpPrev[n - 1]
+
+    def longestPalindromeSubseq6(self, s):
+        n = len(s)
         dp = [[0] *n for _ in range(n)]
-        for i in range(n-1, -1, -1):
+        for i in range(n - 1, -1, -1):
             dp[i][i] = 1
             for j in range(i+1, n ):
                 if s[i] == s[j]:
@@ -54,3 +55,5 @@ class Solution:
             return self.v[(i,j)] 
         return dfs(0, len(s) - 1) 
         
+s = "bbbab"
+print(Solution().longestPalindromeSubseq(s))
