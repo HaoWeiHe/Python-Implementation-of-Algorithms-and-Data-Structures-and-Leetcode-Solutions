94b36f58c0e9415c9fe2fb7726912c7c9176b1df
HaoWeiHe <haoweihohoho@gmail.com>
2022-01-16 16:08:47 -0800

    one pass sol

--- lc_896isMonotonic.py
+++ lc_896isMonotonic.py
@@ -1,4 +1,19 @@
 class Solution(object):
+    def isMonotonic2(self, l):
+        """
+        [1,1,2,2,3]
+       >= 0 
+       or <=0 
+        """
+
+        inc, dec = True, True
+        for a,b in zip(l,l[1:]):
+            if a < b:
+                dec = False
+            if b < a:
+                inc = False
+               
+        return inc or dec
     def isMonotonic(self, l):
         """
         [1,1,2,2,3]
