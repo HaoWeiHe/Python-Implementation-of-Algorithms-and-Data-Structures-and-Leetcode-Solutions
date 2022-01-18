7101ed9fe15e9f7fad4e0288d51e27510c96edf9
HaoWeiHe <haoweihohoho@gmail.com>
2022-01-17 23:21:18 -0800

    concious way

--- lc_647countSubstrings.py
+++ lc_647countSubstrings.py
@@ -1,64 +1,58 @@
-class Solution(object):
-    def countSubstrings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        ans = 0 
-        n = len(s)
-        dp = [[0]*n for _ in range(n)]
-        for i in range(n):
-            dp[i][i] = True
-            ans += 1
-            if i + 1 < n :
-                dp[i][i+1] = s[i] == s[i + 1]
-                ans += s[i] == s[i+1]
+class Solution():
+
+  def countSubstrings(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    ans = 0 
+    n = len(s)
+    dp = [[0]*n for _ in range(n)]
+    
+    for i in range(n-1,-1,-1):
+      for j in range(i, n):
+        if s[i] == s[j] and ( j-i < 2 or dp[i+1][j-1]):
+          dp[i][j] = 1
+          ans += 1
+
+
+    return ans
         
-        for l in range(3,n +1):
-            for i in range(n):
-                j = i + l-1
-                if j >= n:
-                    continue
-                if not s[i] == s[j]:
-                    continue
-                dp[i][j] = dp[i+1][j-1]
-                ans += dp[i][j]
-        return ans
-                
-    def countSubstrings2(self, s):
-        """
-        "abc"
-       i  v
-       j  v
-        aaa
-      i v
-      j   v
-      if odd, cmp first
-      if even, cmp first
-      
-      cbac
-      AxxB
-        """
-        self.v = {}
-        def isPalindromic(s):
-            if s in self.v:
-                return self.v[s]
-            l,r = 0, len(s) -1 
-            while l <r:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
-        c = 0 
-        n = len(s)
-        for i in range(n):
-            for j in range(i, n):
-                target = s[i:j + 1]
-                
-                ans = isPalindromic(target)
-                self.v[target] = ans
-                c += ans
-                
-        return c
-                
+  def countSubstrings2(self, s):
+    """
+    "abc"
+     i  v
+     j  v
+    aaa
+    i v
+    j   v
+    if odd, cmp first
+    if even, cmp first
+    
+    cbac
+    AxxB
+    """
+    self.v = {}
+    def isPalindromic(s):
+      if s in self.v:
+        return self.v[s]
+      l,r = 0, len(s) -1 
+      while l <r:
+        if s[l] != s[r]:
+          return False
+        l += 1
+        r -= 1
+      return True
+    c = 0 
+    n = len(s)
+    for i in range(n):
+      for j in range(i, n):
+        target = s[i:j + 1]
+        
+        ans = isPalindromic(target)
+        self.v[target] = ans
+        c += ans
+        
+    return c
+
+print(Solution().countSubstrings("aaa"))
